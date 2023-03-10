e242d900fb6de38f3b4e1bbc6daca088c0f4afd4
Trinay Bhati <bhatiji780780@gmail.com>
2023-03-11 04:07:15 +0530

    11-march

---
+++ tip calculator js/script.js
@@ -0,0 +1,33 @@
+/*	let billTotal;
+	let tip;
+	let food
+function billAmount(event){
+		 billTotal = Number(event.target.value);
+		 // console.log(billTotal)
+		 let outputTotal = document.getElementById('perPersonTotal');
+		 outputTotal.innerText = `$ ${billTotal}`;
+		}
+function tipPercentage(event){
+	 tip = Number(event.target.value)/100;
+	 food = tip * billTotal
+	 food = food + billTotal;
+	 let outputTotal = document.getElementById('perPersonTotal');
+		 outputTotal.innerText = `$ ${food}`;
+}	
+function increasePeople(){
+
+	let plusButton = document.getElementById('numberOfPeople');
+	plusButton.innerText++;
+}
+
+function decreasePeople(){
+	let plusButton = document.getElementById('numberOfPeople');
+	if(plusButton.innerText <= 1){
+		return;
+	}
+	plusButton.innerText--;
+	
+}
+
+// formula is ((tip/100)*total bill)+total bill  */
+
