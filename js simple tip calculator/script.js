931ea87b4246a3dc404fb3eac44edeb9a0043aca
Trinay Bhati <bhatiji780780@gmail.com>
2023-03-18 23:57:54 +0530

    18-march

--- js simple tip calculator/script.js
+++ js simple tip calculator/script.js
@@ -1,4 +1,4 @@
-/*	let billTotal;
+	let billTotal;
 	let tip;
 	let food
 function billAmount(event){
@@ -29,7 +29,7 @@ function decreasePeople(){
 	
 }
 
-// formula is ((tip/100)*total bill)+total bill  */
+// formula is ((tip/100)*total bill)+total bill  
 
 
 /* 
@@ -54,63 +54,63 @@ These are all the DIV ID's you're gonna need access to 👇
 // Get global access to all inputs / divs here (you'll need them later 😘)
 // bill input, tip input, number of people div, and per person total div
 
-let billInput = document.getElementById('billTotalInput');
-let tipinput = document.getElementById('tipInput');
-let numberOfPeopleDiv = document.getElementById('numberOfPeople');
-let perPersonTotal = document.getElementById('perPersonTotal');
+// let billInput = document.getElementById('billTotalInput');
+// let tipinput = document.getElementById('tipInput');
+// let numberOfPeopleDiv = document.getElementById('numberOfPeople');
+// let perPersonTotal = document.getElementById('perPersonTotal');
 
-// Get number of people from number of people div
-let numberOfPeople = Number(numberOfPeopleDiv.innerText)
+// // Get number of people from number of people div
+// let numberOfPeople = Number(numberOfPeopleDiv.innerText)
 
-// ** Calculate the total bill per person **
-const calculateBill = () => {
-	// console.log(billInput.value);
-	// console.log(numberOfPeople);
+// // ** Calculate the total bill per person **
+// const calculateBill = () => {
+// 	// console.log(billInput.value);
+// 	// console.log(numberOfPeople);
 
-  // get bill from user input & convert it into a number
+//   // get bill from user input & convert it into a number
   
 
-  // get the tip from user & convert it into a percentage (divide by 100)
+//   // get the tip from user & convert it into a percentage (divide by 100)
   
 
-  // get the total tip amount
+//   // get the total tip amount
   
 
-  // calculate the total (tip amount + bill)
+//   // calculate the total (tip amount + bill)
   
 
-  // calculate the per person total (total divided by number of people)
+//   // calculate the per person total (total divided by number of people)
 
 
-  // update the perPersonTotal on DOM & show it to user
+//   // update the perPersonTotal on DOM & show it to user
 
-}
+// }
 
-// ** Splits the bill between more people **
-const increasePeople = () => {
-  // increment the amount of people
-
-
-  // update the DOM with the new number of people
+// // ** Splits the bill between more people **
+// const increasePeople = () => {
+//   // increment the amount of people
 
 
-  // calculate the bill based on the new number of people
+//   // update the DOM with the new number of people
 
-}
 
-// ** Splits the bill between fewer people **
-const decreasePeople = () => {
-  // guard clause
-  // if amount is 1 or less simply return
-  // (a.k.a you can't decrease the number of people to 0 or negative!)
+//   // calculate the bill based on the new number of people
+
+// }
+
+// // ** Splits the bill between fewer people **
+// const decreasePeople = () => {
+//   // guard clause
+//   // if amount is 1 or less simply return
+//   // (a.k.a you can't decrease the number of people to 0 or negative!)
 
   
-  // decrement the amount of people
+//   // decrement the amount of people
 
 
-  // update the DOM with the new number of people
+//   // update the DOM with the new number of people
 
 
-  // calculate the bill based on the new number of people
+//   // calculate the bill based on the new number of people
 
-}
+// }
