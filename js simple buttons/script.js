d648110bc6f5378871151fe804522de633b24ea9
Trinay Bhati <bhatiji780780@gmail.com>
2023-03-19 17:34:00 +0530

    19-march

--- js simple buttons/script.js
+++ js simple buttons/script.js
@@ -1,15 +1,16 @@
 function focusHere(event){
 	event.target.style.border = "10px solid #F9DBBB"
 }
+let inputButton = document.getElementById("input");
+let keyFunction = (e)=>{
+	e.target.style.color = "red";
+	console.log(e.target.value);
+}
+inputButton.addEventListener("keydown", keyFunction)
+
 let submitButton = document.getElementById("btn");
 submitButton.addEventListener("click", (e)=>{
 	let input = Number(document.getElementById("input").value);
-	// if(input === "" || input === " " || input === undefined || input === null ){
-	// 	let errorMessage = document.getElementById("errorMessage");
-	// 	errorMessage.innerText = "can't be left empty";
-	// 	document.getElementById("input").style.border = "10px solid red";
-	// 	console.log("error")
-	// }
 	if(input === "" || input === " " || input === undefined || input === 0 ){
 		let errorMessage = document.getElementById("errorMessage");
 		errorMessage.innerText = "can't be left empty";
@@ -18,13 +19,21 @@ submitButton.addEventListener("click", (e)=>{
 	}
 	else{
 		let box = document.getElementById("box");
-	for(let i = 1; i<input;i++){
-		let element = document.createElement("div");
-		element.textContent = i;
-		element.classList.add(i%2==0 ? "even" : "odd");
-		box.appendChild(element);
+		for(let i = 1; i<input+1;i++){
+			let element = document.createElement("div");
+			element.textContent = i;
+			element.classList.add(i%2==0 ? "even" : "odd") ;
+			box.appendChild(element);
+			element.addEventListener("mouseover", () => {
+				element.style.backgroundColor = "#002B5B";
+			});
+			element.addEventListener("mouseout", () => {
+				element.style.backgroundColor = "#245953";
+			});
 
-	}
+
+		}
+
 	}
 })
 
