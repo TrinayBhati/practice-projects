a69fdf6bdf1d60925f4b50217214eb1b070cd007
Trinay Bhati <bhatiji780780@gmail.com>
2023-05-18 23:28:20 +0530

    18-may

--- js notes app/script.js
+++ js notes app/script.js
@@ -7,26 +7,48 @@ function saveNotes(){
 	notes.forEach((note)=>{
 		data.push(note.value);
 	})
+	// console.log(data);
+	localStorage.setItem("notes", JSON.stringify(data));
 }
 
 addBtn .addEventListener("click", function(){
 	addNote();
 })
 
-function addNote(){
+
+
+
+function addNote(text = ""){
 	const note = document.createElement("div");
 	note.classList.add("note");
 	note.innerHTML = `<div class="tool">
                 <i class="save fas fa-save"></i>
                 <i class="trash fas fa-trash"></i>
             </div>
-            <textarea></textarea> `;       
+            <textarea>${text}</textarea> `;       
     note.querySelector(".trash").addEventListener("click", function(){
     	note.remove();
+    	saveNotes();
     });
     note.querySelector(".save").addEventListener("click", function(){
     	saveNotes();
     });
+    note.querySelector("textarea").addEventListener("focusout",()=>{
+    	saveNotes()
+    })
     main.append(note);
+    saveNotes();
 }
 
+(function(){
+	const lsNotes = JSON.parse(localStorage.getItem("notes"));
+	console.log(lsNotes);
+	if(lsNotes.length === 0){
+		addNote();
+	}
+	lsNotes.forEach((lsNote)=>{
+			addNote(lsNote)
+		})
+	
+
+} )()
